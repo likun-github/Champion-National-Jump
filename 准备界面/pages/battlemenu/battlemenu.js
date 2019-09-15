0c90687f83692f09f727e8c2caba507906d8c621
PlusV <haojiawei199812@gmail.com>
2019-09-15 11:17:11 +0800

    “对战”界面，包括了之前的各种部分，新增了按钮逻辑和动画效果
    Signed-off-by: PlusV <haojiawei199812@gmail.com>

---
+++ 准备界面/pages/battlemenu/battlemenu.js
@@ -0,0 +1,173 @@
+// pages/battlemenu/battlemenu.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+  /*************弹窗***************** */
+  data: {
+    showModal:false,
+    hideModal: true, //模态框1的状态  true-隐藏  false-显示
+    hideModal1: true, //模态框2的状态  true-隐藏  false-显示
+    animationData: {},//动画
+  },
+
+
+  submit: function () {
+    this.setData({
+      showModal: true
+    })
+  },
+
+  preventTouchMove: function () {
+
+  },
+
+
+  close: function () {
+    this.setData({
+      showModal: false
+    })
+  },
+
+  // 显示遮罩层1
+  showModal: function () {
+    var that = this;
+    that.setData({
+      hideModal: false
+    })
+    var animation = wx.createAnimation({
+      duration: 400,//动画的持续时间 默认400ms   数值越大，动画越慢   数值越小，动画越快
+      timingFunction: 'ease',//动画的效果 默认值是linear
+    })
+    this.animation = animation
+    setTimeout(function () {
+      that.fadeIn();//调用显示动画
+    }, 200)
+  },
+
+  // 隐藏遮罩层1
+  hideModal: function () {
+    var that = this;
+    var animation = wx.createAnimation({
+      duration: 400,//动画的持续时间 默认400ms   数值越大，动画越慢   数值越小，动画越快
+      timingFunction: 'ease',//动画的效果 默认值是linear
+    })
+    this.animation = animation
+    that.fadeDown();//调用隐藏动画   
+    setTimeout(function () {
+      that.setData({
+        hideModal: true
+      })
+    }, 720)//先执行下滑动画，再隐藏模块
+
+  },
+
+  // 显示遮罩层2并隐藏遮罩层1
+  showModal1: function () {
+    var that = this;
+    that.setData({
+      hideModal1: false,
+      hideModal: true
+    })
+    var animation = wx.createAnimation({
+      duration: 400,//动画的持续时间 默认400ms   数值越大，动画越慢   数值越小，动画越快
+      timingFunction: 'ease',//动画的效果 默认值是linear
+    })
+    this.animation = animation
+    setTimeout(function () {
+      that.fadeIn();//调用显示动画
+    }, 200)
+  },
+
+  // 隐藏遮罩层2
+  hideModal1: function () {
+    var that = this;
+    var animation = wx.createAnimation({
+      duration: 400,//动画的持续时间 默认400ms   数值越大，动画越慢   数值越小，动画越快
+      timingFunction: 'ease',//动画的效果 默认值是linear
+    })
+    this.animation = animation
+    that.fadeDown();//调用隐藏动画   
+    setTimeout(function () {
+      that.setData({
+        hideModal1: true
+      })
+    }, 720)//先执行下滑动画，再隐藏模块
+
+  },
+
+
+
+  //动画集
+  fadeIn: function () {
+    this.animation.translateY(0).step()
+    this.setData({
+      animationData: this.animation.export()//动画实例的export方法导出动画数据传递给组件的animation属性
+    })
+  },
+  fadeDown: function () {
+    this.animation.translateY(300).step()
+    this.setData({
+      animationData: this.animation.export(),
+    })
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
