2b4d01897489c11d0baacbb0e288cf8a05833311
Laura Chow <Laura_Chow77@163.com>
2019-12-04 22:02:25 +0800

    完成 身份认证弹窗

--- pages/orgmenu/orgmenu.js
+++ pages/orgmenu/orgmenu.js
@@ -4,7 +4,13 @@ const app = getApp()
 
 Page({
   data: {
-    roles: ["student","constitution","coach"],
+    // 认证相关
+    identification: 0, /*0：玩家；1：学生；2：教练；3：机构 */
+    id_selected: 1, /*0：玩家；1：学生；2：教练；3：机构 */
+    show_verification:false,
+
+    // 动画相关
+    hideModal: true,
   },
 
   redict: function(e){
@@ -12,22 +18,35 @@ Page({
     app.redict(e);
   },
 
-  //事件处理函数
+  // 选择认证身份
+  selectIdentification:function(e) {
+    var id = e.currentTarget.id;
+    switch(id){
+      case "student": 
+        this.setData({ id_selected: 1 });
+        break;
+      case "coach":
+        this.setData({ id_selected: 2 });
+        break;
+      case "constitution":
+        this.setData({ id_selected: 3 });
+        break;
+      case "player":
+        this.setData({ id_selected: 0 });
+        break;
+    }
+      
+  },
+
+  // 前往 我的收藏
   goToCollection: function() {
     wx.navigateTo({
       url: '/pages/orgmenu/mycollections/mycollection',
     });
   },
 
-  goToVerification:function() {
-    wx.navigateTo({
-      url: '/pages/orgmenu/verification/constitution',
-    });
-  },
-
-/*
-  // 显示遮罩层
-  showModal: function () {
+  // 选择认证身份  
+  chooseIdentification:function() {
     var that = this;
     that.setData({
       hideModal: false
@@ -35,44 +54,62 @@ Page({
     var animation = wx.createAnimation({
       duration: 600,//动画的持续时间 默认400ms   数值越大，动画越慢   数值越小，动画越快
       timingFunction: 'ease',//动画的效果 默认值是linear
-    })
-    this.animation = animation
+    });
+    this.animation = animation;
     setTimeout(function () {
       that.fadeIn();//调用显示动画
-    }, 200)
+    }, 200);
+    this.setData({ show_verification:true});
+    
   },
 
-  // 隐藏遮罩层
-  hideModal: function () {
+  // 前往身份认证
+  goToVerification:function() {
+    switch(this.data.id_selected) {
+      case 0: /* 玩家 */
+        break;
+      case 1: /* 学生 */
+        break;
+      case 2: /* 教练 */
+        break;
+      case 3: /* 机构 */
+        wx.navigateTo({
+          url: '/pages/orgmenu/verification/constitution',
+        });
+        break;
+    }    
+  },
+
+  cancelVerification:function() {
     var that = this;
     var animation = wx.createAnimation({
       duration: 800,//动画的持续时间 默认400ms   数值越大，动画越慢   数值越小，动画越快
       timingFunction: 'ease',//动画的效果 默认值是linear
-    })
-    this.animation = animation
+    });
+    this.animation = animation;
     that.fadeDown();//调用隐藏动画   
     setTimeout(function () {
       that.setData({
         hideModal: true
-      })
+      });
     }, 720)//先执行下滑动画，再隐藏模块
-
+    this.setData({ show_verification: false });
   },
 
   //动画集
   fadeIn: function () {
-    this.animation.translateY(0).step()
+    this.animation.translateY(0).step();
     this.setData({
       animationData: this.animation.export()//动画实例的export方法导出动画数据传递给组件的animation属性
     })
   },
   fadeDown: function () {
-    this.animation.translateY(300).step()
+    this.animation.translateY(300).step();
     this.setData({
       animationData: this.animation.export(),
     })
   },
-*/
+
 
  onLoad: function () {
    var that = this;
@@ -119,5 +156,4 @@ Page({
       hasUserInfo: true
     })
   }
-
 })
