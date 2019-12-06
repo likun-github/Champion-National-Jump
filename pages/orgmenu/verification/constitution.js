b70da98d10dbf09b9897365174f46abb83ae824f
Laura Chow <Laura_Chow77@163.com>
2019-12-06 23:58:13 +0800

    修改机构认证样式

--- pages/orgmenu/verification/constitution.js
+++ pages/orgmenu/verification/constitution.js
@@ -5,7 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name:"",
+    personInChargeName:"",
+    personInChargePhone: "",
+    images: [],
+    error: 0, /*0：没有错误；1：未填写机构名；2：未填写负责人；3.未填写手机号；4：手机号格式不正确；5.未上传营业执照 */
   },
 
   /**
@@ -13,6 +17,73 @@ Page({
    */
   onLoad: function (options) {
 
+  },
+
+  input:function(e) {
+    switch(e.currentTarget.id) {
+      case "name":
+        this.setData({ name: e.detail.value });
+        break;
+      case "personInChargeName":
+        this.setData({ personInChargeName: e.detail.value });
+        break;
+      case "personInChargePhone":
+        this.setData({ personInChargePhone: e.detail.value });
+        break;
+    } 
+  },
+
+
+  // 
+  changeError:function() {
+    this.setData({ error: 0 });
+  },
+
+
+  chooseImage:function() {
+    var that=this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],  //可选择原图或压缩后的图片
+      sourceType: ['album', 'camera'], //可选择性开放访问相册、相机
+      success: res => {
+        const images = that.data.images.concat(res.tempFilePaths);
+        // 限制最多只能留下3张照片
+        that.data.images = images.length <= 1 ? images : images.slice(0, 1);
+      }
+    });
+  },
+
+  // 提交信息
+  submit:function() {
+    // 未上传营业执照
+    if (this.data.images.length == 0) {
+      this.setData({ error: 5 });
+    } 
+
+    // 手机号格式不正确
+    if (this.data.personInChargePhone.length != 11) {
+      this.setData({ error: 4 });
+    } 
+
+    // 未填写手机号
+    if (this.data.personInChargePhone == "") {
+      this.setData({ error: 3 });
+    } 
+
+    // 未填写负责人名字
+    if (this.data.personInChargeName == "") {
+      this.setData({ error: 2 });
+    } 
+
+    // 未填写机构名
+    if (this.data.name=="") {
+      this.setData({ error: 1 });
+    } 
+
+    // 没有错误，提交表格，并完成页面跳转
+    if(this.data.error == 0) {
+
+    }
   },
 
   /**
