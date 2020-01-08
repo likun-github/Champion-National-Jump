71cf30ed3c04ee6a5157bc27f011c42462b1e4c5
liurongjie <1397608894@qq.com>
2020-01-08 15:03:01 +0800

    hhh

--- app.js
+++ app.js
@@ -5,76 +5,36 @@ var mqtt = require('lab/mqtt.js');
 App({
   onLaunch: function () {
     var that = this;
+    wx.login({
+      complete: (res) => {
+        console.log("又没")
+        that.getuserid(res.code);
+      },
+    })
 
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+   
 
 
 
-    // // 登录
-    // wx.login({
-    //   success: res => {
-    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
-    //     if (res.code) {
-    //       wx.getUserInfo({
-    //         success: function (res) {
-    //           var objz = {};
-    //           objz.avatarUrl = res.userInfo.avatarUrl;
-    //           objz.nickName = res.userInfo.nickName;
-    //           //console.log(objz);
-    //           wx.setStorageSync('userInfo', objz);//存储userInfo
-    //         }
-    //       });
-    //       var d = that.globalData;//这里存储了appid、secret、token串  
-    //       var l = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + d.APPID + '&secret=' + d.APPSECRET + '&js_code=' + res.code + '&grant_type=authorization_code';
-    //       wx.request({
-    //         url: l,
-    //         data: {},
-    //         method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
-    //         // header: {}, // 设置请求的 header  
-    //         success: function (res) {
-    //           var obj = {};
-
-    //           obj.openid = res.data.openid;
-    //           obj.expires_in = Date.now() + res.data.expires_in;
-    //           //console.log(obj);
-    //           wx.setStorageSync('user', obj);//存储openid  
-    //           that.globalData.openId = res.data.openid;
-
-    //           console.log(res.data.openid);
-    //         }
-    //       });
-    //     } else {
-    //       console.log('获取用户登录态失败！' + res.errMsg)
-    //     }
-    //   }
-    // })
-    // // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo
-
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res)
-    //           }
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
+    
 
     // this.GetUserGameInfo();
     this.connect();
    
+
+  },
+  getuserid:function(code){
+    
+    wx.request({
+      url: this.globalData.localhost+'/getuserid',
+      data:{
+        'code':code,
+      },
+      success(res){
+        console.log(res)
+      }
+    })
+    
 
   },
 
@@ -99,7 +59,7 @@ App({
     client.on('connect', (e) => {
       console.log('成功连接服务器111')
       //订阅一个主题
-      client.publish("Test/HD_AddUser", '{"userName":"test1","passWord":"xxx","age":26, "email":"xxxx.com", "tel":151111111}', console.log)
+      client.publish("Test/HD_AddUser", '{"userName":"justtry","passWord":"xxx","age":26, "email":"xxxx.com", "tel":151111111}', console.log)
       client.subscribe('phone_' + 1, { qos: 2 }, function (err) {
         if (!err) {
           //client.publish('123', 'Hello mqtt')
@@ -127,6 +87,7 @@ App({
 
 
   globalData: {
+    localhost:"http://127.0.0.1:8081",
     userInfo: null,
     restServiceBaseUrl: "http://localhost:8082/StayHomeRestServer.NETCoreEnvironment/rest/",
     openId: null,
